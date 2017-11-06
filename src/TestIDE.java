192717effcd2702975db7c879f9ccb059ce86463
Lacertilia <nivmizzet1>
2017-11-06 10:37:47 +0100

    Gissningsprogram

--- src/TestIDE.java
+++ src/TestIDE.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class TestIDE {
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        double rand = Math.random();
+        int random = (int) (rand * 100) + 1;
+        int svar = 0;
+        while(true) {
+            System.out.println("Gissa ett nummer mellan 1 och 100");
+            svar = scan.nextInt();
+            if(svar>random){
+                System.out.println("Talet är mindre.");
+            }else if(svar == random){
+                System.out.println("Du gissade rätt!");
+                System.exit(0);
+            }else if(svar<random){
+                System.out.println("Talet är större.");
+            }
+        }
+
 
     }
 }
